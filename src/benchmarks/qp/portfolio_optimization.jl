28ceb362fa8be3ba7ac73fbc7ce639bc64b77114
goulart-paul <paul.goulart@eng.ox.ac.uk>
2022-09-29 17:19:31 +0100

    add portfolio opt

---
+++ src/benchmarks/qp/portfolio_optimization.jl
@@ -0,0 +1,46 @@
+# Huber fitting.   Uses reformulation at https://osqp.org/docs/examples/portfolio.html
+
+using Random
+
+function portfolio_optimization(model, n, γ = 1)
+
+    rng = Random.MersenneTwister(271324 + n)
+
+    #problem dimensions wrt n
+    p = 0.125
+    k = ceil(Int,0.1 * n)
+    density = 0.5
+
+    F = sprandn(rng,n, k, density)
+    d = rand(rng,n).*sqrt(k) 
+    D = sparse(Diagonal(d))
+    μ = randn(rng,n)
+
+    @variable(model, x[1:n])
+    @variable(model, y[1:k])
+    @constraint(model, y .== F'*x)
+    @constraint(model, sum(x) == 1.)
+    @constraint(model, x .>= 0)
+    @objective(model, Min, x'*D*x + y'*y - 1/(2γ) * (μ'*x))
+
+    optimize!(model)
+
+end
+
+
+#generate problems according to problem type and size 
+
+for n in [100, 1000, 5000]
+    fcn_name = Symbol("portfolio_optimization_n_" * string(n))
+    @eval begin
+        @add_problem qp function $fcn_name(
+            model,
+        )
+            return portfolio_optimization(model,$n)
+        end
+    end
+end
+
+
+
+

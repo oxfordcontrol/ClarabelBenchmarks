0789f2bf4fc3edaeeda7d1b705d0efd7b4623229
goulart-paul <paul.goulart@eng.ox.ac.uk>
2023-07-28 14:55:35 +0100

    add shifted maros

--- src/problem_sets/maros/examples_maros.jl
+++ src/problem_sets/maros/examples_maros.jl
@@ -1,8 +1,27 @@
 function maros_generic(
     model,
-    maros_problem
+    maros_problem,
+    shift = false
 )
     (P, c, Aineq, bineq, Aeq, beq) = maros_load(maros_problem)
+
+    if(shift)
+        P = Matrix(P)
+        while true 
+            try cholesky(P)
+                break
+            catch 
+                #apply shift to P to eliminate 
+                #slightly negative eigenvalues
+                P = Matrix(P)
+                e = eigen(P)
+                c = minimum(e)
+                c = c < 0. ? c : 0.
+                P = sparse(P + (2*(-c + eps()*maximum(e))*I))
+            end
+        end 
+    end
+
     @variable(model, x[1:length(c)])
     @constraint(model, c1, Aineq*x .<= bineq)
     @constraint(model, c2, Aeq*x .== beq)
@@ -14,15 +33,33 @@ function maros_generic(
 end
 
 for test_name in maros_get_test_names()
-    fcn_name = Symbol("maros_" * test_name)
+
+    #shifted versions 
+    fcn_name = Symbol("maros_shifted_" * test_name)
     @eval begin
             @add_problem maros function $fcn_name(
+                model,
+            )
+                return maros_generic(model,$test_name,true)
+            end
+    end
+end 
+
+
+for test_name in maros_get_test_names()
+
+    #original versions 
+    fcn_name = Symbol("maros_" * test_name)
+    @eval begin
+            @add_problem maros_shifted function $fcn_name(
                 model,
             )
                 return maros_generic(model,$test_name)
             end
     end
 end 
+
+
 
 
 

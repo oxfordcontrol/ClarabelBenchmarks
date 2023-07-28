fd2d3dbfedc6d1a1d102c173e87c1104a4d171fd
goulart-paul <paul.goulart@eng.ox.ac.uk>
2023-07-28 10:58:28 +0100

    update plotting, revised benches

--- src/benchmarks/bench_cblib_exp.jl
+++ src/benchmarks/bench_cblib_exp.jl
@@ -2,15 +2,20 @@
 
 # include any solvers you want to test 
 using ClarabelBenchmarks, DataFrames, JLD2
-using Clarabel, ECOS, Gurobi, MosekTools
+using Clarabel, ECOS, Hypatia
+using Gurobi, MosekTools
 using ClarabelRs
 
-solvers = [Mosek,Clarabel,ECOS,ClarabelRs] #, Hypatia]
+solvers = [Mosek,Clarabel,ECOS,ClarabelRs,Hypatia]
+tag     = nothing
 class   = ["cblib_exp"]
 verbose = false
 time_limit = 300.
+rerun = false
+plotlist = [Mosek,ClarabelRs,ECOS,Hypatia]
 
 df = ClarabelBenchmarks.bench_common(
     @__FILE__, solvers, class;
     time_limit = time_limit,
-    verbose = verbose)
+    verbose = verbose, tag = tag, rerun = rerun,
+    plotlist = plotlist)

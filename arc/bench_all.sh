473bc5af873f3224a260caaf69f53d921be741da
Paul Goulart <paul.goulart@eng.ox.ac.uk>
2023-08-27 23:44:42 +0100

    add master script

---
+++ arc/bench_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./bench_cblib_exp.sh
+./bench_cblib_socp.sh
+./bench_dummy.sh
+./bench_maros.sh
+./bench_maros_shifted.sh
+./bench_mpc.sh
+./bench_netlib_feasible.sh
+./bench_netlib_infeasible.sh
+./bench_opf_lp.sh
+./bench_opf_sdp.sh
+./bench_opf_socp.sh
+./bench_sslsq.sh

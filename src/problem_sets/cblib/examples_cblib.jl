a8197ee099ad686b4da321b2f38a9a46136c0917
goulart-paul <paul.goulart@eng.ox.ac.uk>
2023-09-08 11:04:49 +0100

    fix import of cblib files with minus chars

--- src/problem_sets/cblib/examples_cblib.jl
+++ src/problem_sets/cblib/examples_cblib.jl
@@ -12,16 +12,17 @@ end
 
 for group_test_pair in cblib_get_test_names()
 
-    group, test_name = (group_test_pair[1],group_test_pair[2])
+    group, file_name = (group_test_pair[1],group_test_pair[2])
     group_name = "cblib_" * group
 
-    fcn_name   = Symbol(group_name * "_" * test_name )
+    #replace "-" with "_" for functions 
+    fcn_name   = Symbol(group_name * "_" *  replace(file_name,"-" => "_") )
 
     @eval begin
-            @add_problem $group_name $test_name function $fcn_name(
+            @add_problem $group_name $fcn_name function $fcn_name(
                 model,
             )
-                return cblib_generic(model, $group, $test_name)
+                return cblib_generic(model, $group, $file_name)
             end
     end
 end 

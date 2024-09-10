17e6d65c72b93d963848b136efd70c8577b8451d
goulart-paul <paul.goulart@eng.ox.ac.uk>
2024-09-10 16:37:45 +0100

    update solver list to match paper

--- src/benchmarks/bench_maros.jl
+++ src/benchmarks/bench_maros.jl
@@ -2,17 +2,17 @@
 
 # include any solvers you want to test 
 using ClarabelBenchmarks, DataFrames, JLD2
-using Clarabel, ECOS, OSQP, HiGHS, Hypatia
+using Clarabel, ECOS, HiGHS
 using Gurobi, MosekTools
 using ClarabelRs
 
-solvers = [Clarabel,Mosek,ClarabelRs,ECOS,Gurobi,OSQP,Hypatia,HiGHS]
+solvers = [Clarabel,Mosek,ClarabelRs,ECOS,Gurobi,HiGHS]
 tag     = nothing
 class   = "maros"
 verbose = false
 time_limit = 120.
 rerun = false
-plotlist = [Clarabel,Mosek,ClarabelRs,ECOS,Gurobi,Hypatia,HiGHS]
+plotlist = [Clarabel,Mosek,ClarabelRs,ECOS,Gurobi,HiGHS]
 
 
 df = ClarabelBenchmarks.benchmark(
